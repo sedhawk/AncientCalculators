9d390c60c8d57a567a5a91f2167e249672627ea6
CrappyRaizur <razizo41@hotmail.com>
2017-10-04 15:47:29 -0700

    added picture

--- app/src/main/java/cs399/ancientcalculators/MainActivity.java
+++ app/src/main/java/cs399/ancientcalculators/MainActivity.java
@@ -2,8 +2,12 @@ package cs399.ancientcalculators;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +15,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Calculator calc = new Calculator();
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.Op);
+        final Spinner mySpinner = (Spinner) findViewById(R.id.Operators); // the dropdown spinner
+        final EditText inNum1 = (EditText) findViewById(R.id.num1); // the first number user inputs
+        final EditText inNum2 = (EditText) findViewById(R.id.num2); // second number user inputs
+        final TextView answer = (TextView) findViewById(R.id.ans); // the answer block
+
+/*
+        // transforming the edit text to a double
+        String num1Value = inNum1.getText().toString(); // num1
+        final Double num1Final =Double.parseDouble(num1Value);
+
+        String num2Value = inNum1.getText().toString(); // num2
+        final Double num2Final =Double.parseDouble(num2Value);
+*/
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.operators));
 
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+
+
+        Button go = (Button) findViewById(R.id.go);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    answer.setText("Hello");
+
+
+                } catch(Exception e) { e.printStackTrace(); }
+
+
+            }
+        });
+
+
+
     }
     public static void main(String[] args){
 
